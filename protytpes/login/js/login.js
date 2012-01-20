7714cd7b6dc3142599973593ce281cda2734fcac
Sascha Lange <sascha77@googlemail.com>
2012-01-20 14:13:45 +0100

    redirect problem with safari

--- protytpes/login/js/login.js
+++ protytpes/login/js/login.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
   
+  var currentUser = null;
+  var clientLosesAuthHeaderOnRedirect = true;
+  
+  $(document).bind('ajaxSend', function(event, xhr) {
+    if (currentUser && currentUser['access_token']) {
+      if (clientLosesAuthHeaderOnRedirect) {
+        
+      }
+      else {
+        var token = (currentUser && currentUser['access_token']) ? currentUser['access_token'] : "";
+        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+      }
+    }
+    xhr.setRequestHeader('Accept', 'application/json');
+  });
+  
+  var setCurrentUser = function(data) {
+    currentUser = data;
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3000/identities/self',
+      success: function(data, textStatus, jqXHR) {
+        alert($.param(data));
+      },
+    });
+  };
+  
   var attachClear = function() {
     $(this)
       .data('default', $(this).val())
@@ -25,17 +52,20 @@ $(document).ready(function() {
 		        <input type="password" name="password" id="password" value="" class="clear" />\
 	        	<label for="password">Password</label>\
           </div>\
+          <div id="login-message"></div>\
 	      </fieldset>\
 	      </form>\
       </div>');
       
-    $('#input-password').hide();
-
       
     $('#login-dialog input.clear').each(function() {
       $(this)
         .keydown(function(event) {               // necessary to remove label immediately for the case, where the user holds the key
-          if (!event.keyCode in { 13:true, 8:true, 9:true, 16: true }) {
+          if (event.keyCode != 13 &&
+              event.keyCode != 8 &&
+              event.keyCode != 9 &&
+              event.keyCode != 16
+            ) {
             $(this).siblings('label').hide();
           }
         })
@@ -70,14 +100,41 @@ $(document).ready(function() {
       {
         text: 'Send',
         click: function() {
-          alert($('#login-form').serialize());
+          if ($('#login-form #username').val() == '' ||
+              $('#login-form #password').val() == '') {
+            $('div#login-message').html('Please provide your credentials.');
+            return ;
+          }
+          var params = $('#login-form').serializeArray();
+          params.push({
+            name: 'client_id',
+            value: 'XYZ'
+          });
+          params.push({
+            name: 'scope',
+            value: '5dentity wackadoo'
+          });
+          params.push({
+            name: 'grant_type',
+            value: 'password'
+          });
+          $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/oauth2/access_token',
+            data: params ,
+            success: function(data, textStatus, jqXHR) {
+              if (data['access_token']) {
+                setCurrentUser(data);
+                $('#login-dialog').dialog('close');
+              }
+            }
+          });
         }
       }
       ]
     });
-  
-    $('.ui-dialog .text').blur();
-    
+
+    $('input').blur();
     
   };
   
