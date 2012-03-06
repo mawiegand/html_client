504182ce8455bc8d74657675594b86158aa96541
Sascha Lange <sascha77@googlemail.com>
2012-03-06 16:14:02 +0100

    added a usefull instance methods to Map.node

--- client/js/awe/map/maptree.js
+++ client/js/awe/map/maptree.js
@@ -8,17 +8,63 @@ var AWE = window.AWE || {};
 AWE.Map = AWE.Map || function() {
   
   return {
+    
+    /** creates a node-object from the given spec, or, if no specs given, sets
+     * all values to defaults appropriate for a root node. Spec could be JSON
+     * data send by the game_server and could contain the spec for a complete
+     * subtree. In this case, this method creates child-nodes as needed and
+     * returns a complete subtree matching the spec. */
     node: function(spec) {
       var that = {};
       
       var _updated_at = spec.updated_at || null;
       var _created_at = spec.created_at || null;
       
-      var _leaf = spec.leaf || null;
+      var _leaf = spec.leaf || true;
       var _level = spec.level || 0;
       var _parent = null;
       
       var _children = null;
+      
+      /** true, in case the node is a leaf-node (has no children) */
+      that.isLeaf = function() {
+        return _leaf;
+      };
+      
+      /** returns the parent (next ancestor) of this node or null for a root
+       * node. */
+      that.parent = function () {
+        return _parent;
+      };
+      
+      /** sets the parent of the node. Should be used with caution; you need
+       * to insert the node to the parent's children manually, when using this
+       * method. */
+      that.setParent = function(newParent) {
+        _parent = newParent;
+      };
+      
+      that.children = function() {
+        return _children;
+      };
+      
+      that.child = function(qtPath) {
+        if (!_children) return null;
+        return _children[qtPath];
+      };
+      
+      that.setChild = function(qtPath, childNode) {
+        if (!_children) {
+          _children = [ null, null, null, null ];
+          _leaf = false;
+        }
+        _children[qtPath] = childNode;
+      };
+      
+      that.insertAsChild = function(qtPath, childNode) {
+        that.setChild(qtPath, childNode);
+        childNode.setParaent(that);
+      };
       
       return that;
     },
