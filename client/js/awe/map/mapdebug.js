7eb6bf95676ee8456a99a95bedda167d020ff88d
Your Name <p@trick-fox.de>
2012-03-13 17:21:45 +0100

    changed to loose augmented module

--- client/js/awe/map/mapdebug.js
+++ client/js/awe/map/mapdebug.js
@@ -8,43 +8,42 @@ var AWE = window.AWE || {};
 
 
 
-AWE.MapDebug = AWE.MapDebug || function() {
-    
-  return {
-    showTree: function(rootNode, width, levels) {
-      var classStr = '';
-          
-/*    if (!rootNode) {
-        return "<div class='subtree' style='width: " + width + "px; height: "+width+"px; border: solid black "+(levels+1)+"px; margin: -"+(levels+1)+"px; display: inline-block; text-align: center; overflow: visible;'>?</div>"
-      }*/
-          
-      if (rootNode.isLeaf()) {
-        classStr = 'class="leaf"';
-      }
-      else if (levels == 0 || !rootNode.children() || !rootNode.child(0)) {
-        classStr = 'class="subtree"';
-      }
-      
-      var c = parseInt(rootNode.path().substring(rootNode.path().length-1));
-    
-   //   var string = "<div " + classStr + " style='vertical-align=middle; width: " + width + "px; height: "+width+"px; border: solid black "+(levels+1)+"px; margin: -"+(levels+1)+"px; display: inline-block; text-align: center; overflow: visible; " + (rootNode.isLeaf() ? 'font-weight: bold;' : '') + "'>"
-      var string = "<div " + classStr + " style='position:absolute; top: "+ (!c || c<2 ? 0 : width ) +"px; left: "+ (!c || c%2 == 0 ? 0 : width) + "px; width: " + width + "px; height: "+width+"px; border: solid black "+(levels+1)+"px; margin: -"+(levels+1)+"px; display: block; text-align: center; overflow: visible; " + (rootNode.isLeaf() ? 'font-weight: bold;' : '') + "'>"
-
-      if (rootNode.isLeaf() || levels == 0 || !rootNode.children() || !rootNode.child(0)) {
-        string += rootNode.path();
-      }
-      else {
-        for (var i=0; i < 4; i++) {
-          string += AWE.MapDebug.showTree(rootNode.child(i), width/2, levels-1);
-        }
-      }
-      string += "</div>"
-      
-      return string;
-    },
-  };
+AWE.MapDebug = function(module) {
   
-}();
+
+  module.showTree = function(rootNode, width, levels) {
+    var classStr = '';
+
+    /*    if (!rootNode) {
+     return "<div class='subtree' style='width: " + width + "px; height: "+width+"px; border: solid black "+(levels+1)+"px; margin: -"+(levels+1)+"px; display: inline-block; text-align: center; overflow: visible;'>?</div>"
+     }*/
+
+    if(rootNode.isLeaf()) {
+      classStr = 'class="leaf"';
+    } else if(levels == 0 || !rootNode.children() || !rootNode.child(0)) {
+      classStr = 'class="subtree"';
+    }
+
+    var c = parseInt(rootNode.path().substring(rootNode.path().length - 1));
+
+    //   var string = "<div " + classStr + " style='vertical-align=middle; width: " + width + "px; height: "+width+"px; border: solid black "+(levels+1)+"px; margin: -"+(levels+1)+"px; display: inline-block; text-align: center; overflow: visible; " + (rootNode.isLeaf() ? 'font-weight: bold;' : '') + "'>"
+    var string = "<div " + classStr + " style='position:absolute; top: " + (!c || c < 2 ? 0 : width ) + "px; left: " + (!c || c % 2 == 0 ? 0 : width) + "px; width: " + width + "px; height: " + width + "px; border: solid black " + (levels + 1) + "px; margin: -" + (levels + 1) + "px; display: block; text-align: center; overflow: visible; " + (rootNode.isLeaf() ? 'font-weight: bold;' : '') + "'>"
+
+    if(rootNode.isLeaf() || levels == 0 || !rootNode.children() || !rootNode.child(0)) {
+      string += rootNode.path();
+    } else {
+      for(var i = 0; i < 4; i++) {
+        string += AWE.MapDebug.showTree(rootNode.child(i), width / 2, levels - 1);
+      }
+    }
+    string += "</div>"
+
+    return string;
+  };
+
+  return module;
+  
+}(AWE.MapDebug || {});
 
 
 $(document).ready(function() {
