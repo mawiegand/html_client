328655233e4b14c5842249a77a91219c03bd722b
Your Name <p@trick-fox.de>
2012-03-23 15:14:55 +0100

    Comments and restructuring

--- client/js/awe/util/extensions.js
+++ client/js/awe/util/extensions.js
@@ -11,30 +11,45 @@ AWE.Ext = (function(module) {
   /** returns the next absolutely larger integer; that is returns
    * the floor for negative numbers and the ceil for positive numbers. This method
    * is used to enlarge an area with positive or negative coordinates to full integers
-   * in such a way that it encloses the original area. */
+   * in such a way that it encloses the original area.
+   */
   Number.prototype.extendInteger = function() {
     return Math[(this > 0 ? 'ceil' : 'floor')](this);
   };
   
+  /** check if _obj is an Array
+   */
   module.isArray = function(_obj) {
     return _obj && typeof _obj === 'object' && _obj.constructor === Array;
   };
   
-  
+  /** if _obj is an array, applyFunction calls _function multiple times with every
+   * array member as parameter, else _function will be called once with _obj as parameter
+   */
   module.applyFunction = function(_obj, _function) {
     if (module.isArray(_obj)) {
       for (var o in _obj) {
-       // if (typeof _obj[o] === 'object') {
-          _function(_obj[o]);
-      //  }
+        _function(_obj[o]);
       }
     }
     else if (_obj) {
-     // if (typeof _obj === 'object') {
-          _function(_obj);
-    //  }
+      _function(_obj);
     }
-  }      
+  }
+    
+  /** returns a random integer in [0;max[
+   */ 
+  module.random = function(max) {
+    return Math.floor(Math.random() * max);
+  };
+  
+  /** the signum function
+   */
+  module.sign = function(x) {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
+  };
 
   return module;
   
