498f852e2a0b0c764320ae1d5baa227aa939cd98
Sascha Lange <sascha77@googlemail.com>
2012-03-30 14:23:30 +0200

    made hashable properties a partial

--- client/js/awe/util/partials.js
+++ client/js/awe/util/partials.js
@@ -8,6 +8,10 @@ var AWE = window.AWE || {};
 
 AWE.Partials = (function(module) {
   
+  module.PROPERTY_READ_WRITE = 0;
+  module.PROPERTY_READ_ONLY = 1;
+  module.PROPERTY_HASHABLE = true;
+  
   module.addUpdateTracking = function(obj) {
     var isUpdating = false;
     
@@ -37,6 +41,135 @@ AWE.Partials = (function(module) {
       _lastChange = new Date();
     };
   };
+  
+  
+  module.addProperties = function(obj, hook, my) {
+        
+    /** creates an auto-updated hash for a property of the entity type. */
+    var createAccessHashForAttribute = function(attribute) {
+      var upperCaseAttr = attribute.charAt(0).toUpperCase() + attribute.slice(1);
+
+      if (hook.accessHashes === undefined) {
+        hook.accessHashes = {};
+      }
+
+      if (!hook.accessHashes[attribute]) {
+
+        hook.accessHashes[attribute] = (function() {
+          var hash = {};
+      
+          hash.allEntries = {};
+      
+          /** use with caution (overwrites for example armies that have moved
+           * to another region). When fetching all entities for a value of an 
+           * attribute from the server, all received entities are added 
+           * automatically to the corresponding hash; so there's no need to
+           * set its values manually, using this method. */
+          hash.setEntriesForValue = function(val, newEntries, timestamp) {
+            timestamp = timestamp || new Date();
+            this.allEntries[val] = { entries: newEntries, lastUpdateAt: timestamp };
+          };
+          hash.addEntry = function(entry) {
+            if (!this.allEntries[entry[attribute]()]) {
+              this.allEntries[entry[attribute]()] = { entries: {}, lastUpdateAt: new Date(1970) };
+            }
+            this.allEntries[entry[attribute]()].entries[entry.id()] = entry;
+          };
+          hash.removeEntry = function(entry) {
+            if (this.allEntries[entry[attribute]()] && this.allEntries[entry[attribute]()].entries[entry.id()]) {
+              delete this.allEntries[entry[attribute]()].entries[entry.id()];
+            }
+          }
+          hash.getEntriesForValue = function(val) {
+            if (this.allEntries[val]) {
+              return this.allEntries[val].entries;
+            }
+            else {
+              return {};
+            }
+          }
+          hash.setLastUpdateAtForValue = function(val, timestamp) {
+            timestamp = timestamp || new Date();
+            if (this.allEntries[val]) {
+              this.allEntries[val].lastUpdateAt = timestamp;
+            }
+            else {
+              this.allEntries[val] = { entries: {}, lastUpdateAt: timestamp };
+            }
+          }
+          hash.lastUpdateForValue = function(val, timestamp) {
+            if (this.allEntries[val]) {
+              return this.allEntries[val].lastUpdateAt; 
+            }
+            else {
+              return new Date(1970);
+            }
+          }
+          hash.wasLastUpdateForValueAfter = function(val, timestamp) {
+            return this.allEntries[val] && this.allEntries[val].lastUpdateAt >= timestamp;
+          }
+      
+          return hash;
+        }());
+        
+        hook['getAllFor'+upperCaseAttr] = function(value) {
+          return hook.accessHashes[attribute].getEntriesForValue(value);
+        }
+        
+        hook['lastUpdateFor'+upperCaseAttr] = function(value) {
+          return hook.accessHashes[attribute].lastUpdateForValue(value);
+        }
+        
+        hook['wasLastUpdateFor'+upperCaseAttr+'After'] = function(value, timestamp) {
+          return hook.accessHashes[attribute].wasLastUpdateForValueAfter(value, timestamp);
+        }
+        
+        hook['accessHashFor'+upperCaseAttr] = function() {
+          return hook.accessHashes[attribute];
+        }
+      }
+    }      
+    
+  
+    /** creates a property that gives to a newly created attribute of the my 
+     * object. Can be used to create attribute, setter and getter with just
+     * one line of code in any derived object. */
+    obj.property = function(attribute, defaultValue, access, hashable) {
+      if (defaultValue === undefined) {
+        defaultValue = null;
+      }
+      access = access || module.PROPERTY_READ_WRITE;
+      hashable = hashable || false;
+   
+      var setterString = 'set'+attribute.charAt(0).toUpperCase() + attribute.slice(1);
+  
+      my[attribute] = defaultValue;                          // attribute
+      if (!hashable) {                                       // simple protected setter
+        my[setterString] = function(val) { my[attribute] = val; }
+      }
+      else {                                                 // setter, that also updates the hash
+        if (!hook.accessHashes || !hook.accessHashes[attribute]) {//   create hash, if not already there
+          createAccessHashForAttribute(attribute); 
+        }
+        
+        my[setterString] = function(val) {     
+          if (my[attribute] != val) {
+            if (my[attribute]) {
+              hook.accessHashes[attribute].removeEntry(obj);
+            }
+            my[attribute] = val; 
+            if (val) {
+              hook.accessHashes[attribute].addEntry(obj);
+            }
+          }
+        }        
+      } 
+      this[attribute] = function() { return my[attribute]; } // getter
+      if (access === module.PROPERTY_READ_WRITE) {           // public setter
+        this[setterString] = function(val) { my[setterString](val); }
+      }
+    };
+  }
   
   return module;
 }(AWE.Partials || {}));
