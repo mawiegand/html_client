a55ae961a78a877f292d76decde9a7de862e8776
Sascha Lange <sascha77@googlemail.com>
2012-04-12 18:17:18 +0200

    label to info instead of inventory

--- client/js/awe/UI/armyinspectorview.js
+++ client/js/awe/UI/armyinspectorview.js
@@ -169,7 +169,7 @@ AWE.UI = (function(module) {
       }
 
       _invButtonView = AWE.UI.createButtonView();
-      _invButtonView.initWithControllerTextAndImage(controller, 'Inventory', AWE.UI.ImageCache.getImage("map/button1"));
+      _invButtonView.initWithControllerTextAndImage(controller, 'Info', AWE.UI.ImageCache.getImage("map/button1"));
       _invButtonView.setFrame(AWE.Geometry.createRect(180, 0, 48, 48));
       _invButtonView.onClick = function() { that.onInventoryButtonClick(_army) };
       _container.addChild(_invButtonView.displayObject());
