73b54239b549831f01d24c1094d28d2c6cfa5265
Your Name <p@trick-fox.de>
2012-06-07 19:58:42 +0200

    Activity Indicator View

---
+++ client/js/awe/UI/ember/activityindicatorview.js
@@ -0,0 +1,26 @@
+/* Authors: Sascha Lange <sascha@5dlab.com>, 
+ *          Patrick Fox <patrick@5dlab.com>
+ * Copyright (C) 2012 5D Lab GmbH, Freiburg, Germany
+ * Do not copy, do not distribute. All rights reserved.
+ */
+
+var AWE = AWE || {};
+AWE.UI = AWE.UI || {}; 
+
+AWE.UI.Ember = (function(module) {
+  
+  module.ActivityIndicatorView = Ember.View.extend({
+    tagName: 'img',
+    attributeBindings: ['width', 'height', 'src'],
+    width: '240',
+    heightBinding: 'width',
+    src: "images/icons/activityindicator.svg",
+  });  
+
+  return module;
+    
+}(AWE.UI.Ember || {}));
+
+
+
+
