9675c5a4b862e17f035e030a625b716b8b252005
Your Name <p@trick-fox.de>
2012-07-10 01:27:57 +0200

    typos

--- client/app/assets/javascripts/awe-ui.js
+++ client/app/assets/javascripts/awe-ui.js
@@ -1,7 +1,7 @@
-//= require awe/ui/view
-//= require awe/ui/ember/domview
-//= require awe/ui/ember/standardviews
-//= require awe/ui/ember/settlementviews
-//= require awe/ui/ember/buildingviews
+//= require awe/UI/view
+//= require awe/UI/ember/domview
+//= require awe/UI/ember/standardviews
+//= require awe/UI/ember/settlementviews
+//= require awe/UI/ember/buildingviews
 
-//= require_tree ./awe/ui
+//= require_tree ./awe/UI
