00f8b56068f7d18ace9d117f7e1ee66b340bfc75
Your Name <p@trick-fox.de>
2012-07-10 01:24:53 +0200

    added absolute assets path

--- client/config/environments/production.rb
+++ client/config/environments/production.rb
@@ -15,10 +15,12 @@ Client::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.action_controller.asset_host = "https://wack-a-doo.de/client"  
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
