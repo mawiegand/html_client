b44a42c69fcee0ac696974f441f7158b322e1e0b
Sascha Lange <sascha77@googlemail.com>
2012-08-21 17:09:45 +0200

    always auto-update message inbox

--- client/app/assets/javascripts/awe/gamestate/messaging.js
+++ client/app/assets/javascripts/awe/gamestate/messaging.js
@@ -377,6 +377,37 @@ AWE.GS = (function(module) {
     that.setLastUpdateForCharacter = function(characterId, timestamp) {
       module.InboxAccess.accessHashForOwner_id().setLastUpdateAtForValue(characterId, timestamp);
     };
+    
+    that.triggerInboxAutoUpdate = function() {
+      var character = AWE.GS.CharacterManager.getCurrentCharacter();
+      if (!character) return ;
+      
+      var inbox = character.get('inbox');
+      
+      if (!inbox) {
+        character.fetchInbox(function(inboxes, status) {
+          if (status === AWE.Net.NOT_FOUND || !inboxes) {
+            console.log('ERROR: inbox of current character not found on server.');
+          } 
+          else { 
+            inbox = AWE.GS.CharacterManager.getCurrentCharacter().get('inbox');
+            if (inbox) {
+              inbox.fetchEntries();
+            }
+            else {
+              console.log('ERROR: inbox could not be fetched from server.');
+            }
+          }
+        });
+      }
+      else if (inbox && inbox.lastUpdateAt(AWE.GS.ENTITY_UPDATE_TYPE_FULL).getTime() + 10000 < new Date().getTime()) { // timeout
+        AWE.GS.InboxManager.updateMessageBox(inbox.get('id'), AWE.GS.ENTITY_UPDATE_TYPE_FULL, function(inbox, status) {
+					if (inbox && inbox.getId() && status !== AWE.Net.NOT_MODIFIED) {
+            inbox.fetchEntries()
+					}
+        });
+      }
+    };
   
     return that;
       
