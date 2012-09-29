7744c8409a834104683218bd368e97b7403d81f1
Sascha Lange <sascha77@googlemail.com>
2012-09-29 18:28:30 +0200

    more information, first estimates

--- client/app/assets/javascripts/awe/util/timecorrection.js
+++ client/app/assets/javascripts/awe/util/timecorrection.js
@@ -21,14 +21,28 @@ AWE.Util.TimeCorrection = (function(module) {
     my = my || {}; 
     
     my.remoteLag = null;
-    my.alpha     = 0.95;
+    my.alpha     = 0.92;
     
     
     that = {};  
     
-    /** requestEndTime argument is optional. */
+    that.estimatedLag = function() {
+      return Math.abs(my.remoteLag || 0.0) > 1.0 ? (my.remoteLag || 0.0) : 0.0;
+    };
+    
+    that.estimatedServerTime = function() {
+      
+    };
+    
+    /** requestEndTime argument is optional. 
+     * remoteTime has SECONDS precision (last 3 places always 000).
+     * local times are measured in MS precision (might even have fractions). 
+     * THUS: A SINGLE MEASUREMENT MAY BE OFF BY UP TO 1second (usually averages out).
+     */
     that.registerMeasurement = function(remoteTime, localTime, requestEndTime) {
       requestEndTime = requestEndTime || localTime;
+
+      // TODO: if duration was > 1s and we already have a estimate of the lag, just ignore this measurement as it will be to imprecise
 
       var duration   = requestEndTime.getTime()-localTime.getTime();
       var normalized = localTime.getTime()+duration/2.0;
@@ -41,10 +55,10 @@ AWE.Util.TimeCorrection = (function(module) {
         console.log('TIME_CORRECTION NEW_LAG', newLag, 'duration', duration, 'normalized', normalized, 'difference', difference, 'lag before', my.remoteLag);
       }
       if (AWE.Config.TIME_DEBUG_LEVEL >= AWE.Config.DEBUG_LEVEL_INFO) {      
-        console.log('LAG', 
-                    'HOURS:', Math.floor((newLag / (1000*60*60))),   
-                    'MINUTES:', Math.floor((newLag / (1000*60)))%60,   
-                    'SECONDS:', Math.floor((newLag / 1000))%60,   
+        console.log('LAG: '+(newLag < 0.0 ? "-" : ""),
+                    'HOURS:',   Math.floor(Math.abs(newLag / (1000*60*60))),   
+                    'MINUTES:', Math.floor(Math.abs(newLag / (1000*60)))%60,   
+                    'SECONDS:', Math.floor(Math.abs(newLag / 1000))%60,   
                     'MS:', newLag % 1000);
       }
       my.remoteLag   = newLag;
