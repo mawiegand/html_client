7176d974abcc59a85f2fd59e2a97ae3ff08c0429
Patrick <p@trick-fox.de>
2012-12-05 01:54:46 +0100

    enter own base info dialog on double click

--- client/app/assets/javascripts/awe/UI/baseview.js
+++ client/app/assets/javascripts/awe/UI/baseview.js
@@ -110,6 +110,8 @@ AWE.UI = (function(module) {
         _imageView.setContentMode(module.ViewContentModeNone);
         _imageView.setFrame(AWE.Geometry.createRect(0, 0, AWE.Config.MAPPING_FORTRESS_SIZE, AWE.Config.MAPPING_FORTRESS_SIZE));
         _imageView.onClick = that.onClick;
+        _imageView.onDoubleClick = that.onDoubleClick;
+        _imageView.onDoubleClick = that.onDoubleClick;
         _imageView.onMouseOver = that.onMouseOver;
         _imageView.onMouseOut = that.onMouseOut;
         this.addChildAt(_imageView, 0);
@@ -147,6 +149,7 @@ AWE.UI = (function(module) {
         _battleView.initWithControllerAndImage(my.controller, AWE.UI.ImageCache.getImage('map/army/battle'));
         _battleView.setFrame(AWE.Geometry.createRect(0, -50, 65, 65));
         _battleView.onClick = that.onClick;
+        _battleView.onDoubleClick = that.onDoubleClick;
         _battleView.onMouseOver = that.onMouseOver;
         _battleView.onMouseOut = that.onMouseOut;
         this.addChild(_battleView);
@@ -161,6 +164,7 @@ AWE.UI = (function(module) {
         _suspensionView.initWithControllerAndImage(my.controller, AWE.UI.ImageCache.getImage('map/army/suspended'));
         _suspensionView.setFrame(AWE.Geometry.createRect(0, -50, 65, 65));
         _suspensionView.onClick = that.onClick;
+        _suspensionView.onDoubleClick = that.onDoubleClick;
         _suspensionView.onMouseOver = that.onMouseOver;
         _suspensionView.onMouseOut = that.onMouseOut;
         this.addChild(_suspensionView, 0);
@@ -196,6 +200,14 @@ AWE.UI = (function(module) {
     
     that.army = function() {
       return _location.garrisonArmy();
+    };
+
+    that.onDoubleClick = function() {
+      if (AWE.GS.player.getPath('currentCharacter.id') == _location.ownerId()) {
+        WACKADOO.activateBaseController({
+          locationId: _location.id()
+        });
+      }
     };
 
     return that;
