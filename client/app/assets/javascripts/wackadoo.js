a674e4ba5dc7755d05472d280f9cd0c46c0ca478
salange <sascha@5dlab.com>
2013-05-12 18:20:00 +0200

    use ssl instead of http

--- client/app/assets/javascripts/wackadoo.js
+++ client/app/assets/javascripts/wackadoo.js
@@ -166,7 +166,7 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
       HOST_PUBSUB    = "pubsub."    +base;
       HOST_VJUD      = "vjud."      +base;
       HOST_ANONYMOUS = "anonymous." +base;
-      HOST_BOSH      = "http://"+base+"/http-bind/";
+      HOST_BOSH      = "https://"+base+"/http-bind/";
       
       var character = AWE.GS.game && AWE.GS.game.get('currentCharacter');
       var beginner  = character && character.get('beginner');
