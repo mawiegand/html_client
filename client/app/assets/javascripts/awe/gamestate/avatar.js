c9d1aa9dda26d78868953c8c440f72f14acde7e8
Christian Wansart <christian@5dlab.com>
2013-06-14 12:26:02 +0200

    fixed part names (plural forms required)

--- client/app/assets/javascripts/awe/gamestate/avatar.js
+++ client/app/assets/javascripts/awe/gamestate/avatar.js
@@ -20,34 +20,36 @@ AWE.GS = function (module) {
     /* TODO: read from AWE Rules */
     avatar_rules: {
       chars: {
-        gender:   1,
-        chain:    1,
-        eyes:     2,
-        hair:     2,
-        mouth:    2,
-        head:     1,
-        beard:    2,
-        veilchen: 1,
-        tattoo:   2,
+        gender:    1,
+        chains:    1,
+        eyes:      2,
+        hairs:     2,
+        mouths:    2,
+        heads:     1,
+        beards:    2,
+        veilchens: 1,
+        tattoos:   2,
       },
       offset: {
-        gender:     0,
-        chain:      1,
-        eyes:       2,
-        hair:       4,
-        mouth:      6,
-        head:       8,
-        beard:      9,
-        veilchen:  11,
-        tattoo:    12,
+        gender:      0,
+        chains:      1,
+        eyes:        2,
+        hairs:       4,
+        mouths:      6,
+        heads:       8,
+        beards:      9,
+        veilchens:  11,
+        tattoos:    12,
       },
     },
 
-    init: function() {
-      this.set('avatar_string', 'm010300500');
-    },
+    /*init: function() {
+      this.set('avatar_string', 'm0010102102102');
+      //this.set('avatar_string', 'f1010101100002');
+    },*/
 
     getPart: function(part) {
+      /* TODO: check if avatar_rules has a key "part" */
       var rules  = this.get('avatar_rules');
       var result = "";
       var cursor = rules.offset[part];
@@ -57,7 +59,9 @@ AWE.GS = function (module) {
       for(var i = 0; i < chars; ++i) {
         result += a_str[cursor++];
       }
-      return result;
+
+      if(!isNaN(result)) result = parseInt(result);
+      return result; 
     },
 
   });
