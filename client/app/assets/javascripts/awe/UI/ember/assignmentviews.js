aa6bc598bdc892ce1db5599d0259ba0c3c51540b
salange <sascha@5dlab.com>
2013-08-29 20:48:12 +0200

    tavern and assignments

--- client/app/assets/javascripts/awe/UI/ember/assignmentviews.js
+++ client/app/assets/javascripts/awe/UI/ember/assignmentviews.js
@@ -80,7 +80,7 @@ AWE.UI.Ember = function(module) {
       var jobInterval     = Date.parseISODate(this.getPath('assignment.ended_at')).getTime() - Date.parseISODate(this.getPath('assignment.started_at')).getTime();
       var progression = jobInterval != 0 ? currentInterval / jobInterval : -1;
       progression = progression < 0 ? 0 : (progression > 1 ? 1 : progression);
-      return 'width: ' + Math.ceil(300 * progression) + 'px;';
+      return 'width: ' + Math.ceil(250 * progression) + 'px;';
     }.property('timeRemaining').cacheable(),
 
     duration: function() {
@@ -108,6 +108,10 @@ AWE.UI.Ember = function(module) {
         self.set('halving', false);
       });
       return false;
+    },
+
+    togglePressed: function() {
+      this.set('collapsed', !this.get('collapsed'));
     },
 
     collapsePressed: function() {
@@ -276,7 +280,7 @@ AWE.UI.Ember = function(module) {
       var jobInterval     = Date.parseISODate(this.getPath('specialAssignment.ended_at')).getTime() - Date.parseISODate(this.getPath('specialAssignment.started_at')).getTime();
       var progression = jobInterval != 0 ? currentInterval / jobInterval : -1;
       progression = progression < 0 ? 0 : (progression > 1 ? 1 : progression);
-      return 'width: ' + Math.ceil(300 * progression) + 'px;';
+      return 'width: ' + Math.ceil(250 * progression) + 'px;';
     }.property('timeRemaining').cacheable(),
 
     duration: function() {
@@ -304,6 +308,10 @@ AWE.UI.Ember = function(module) {
         self.set('halving', false);
       });
       return false;
+    },
+
+    togglePressed: function() {
+      this.set('collapsed', !this.get('collapsed'));
     },
 
     collapsePressed: function() {
