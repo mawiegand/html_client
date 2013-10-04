5462707181e6b50dab841035e3c2691f4db9486f
salange <sascha@5dlab.com>
2013-10-04 20:58:52 +0200

    correct fb player property name

--- client/app/assets/javascripts/awe/facebook/fb_loader.js
+++ client/app/assets/javascripts/awe/facebook/fb_loader.js
@@ -73,7 +73,7 @@ AWE.Facebook = (function(module) {
        var js, fjs = d.getElementsByTagName(s)[0];
        if (d.getElementById(id)) {return;}
        js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/'+sdkLocale+'/all.js";
+       js.src = "//connect.facebook.net/"+sdkLocale+"/all.js";
        fjs.parentNode.insertBefore(js, fjs);
      }(document, 'script', 'facebook-jssdk'));
   }
