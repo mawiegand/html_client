57f968fe83f59935a916784822b49911b5755bc3
Patrick <p@trick-fox.de>
2013-10-08 11:20:10 +0200

    removed semicolons

--- client/app/controllers/canvas_controller.rb
+++ client/app/controllers/canvas_controller.rb
@@ -5,11 +5,11 @@ class CanvasController < ApplicationController
   def show
     @title = "Wack-A-Doo"
     
-    @app_id = "127037377498922";
-    @canvas_page = "http://apps.facebook.com/wack-a-doo";
-    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{ @app_id }&redirect_uri=#{ URI::escape(@canvas_page) }";
+    @app_id = "127037377498922"
+    @canvas_page = "http://apps.facebook.com/wack-a-doo"
+    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{ @app_id }&redirect_uri=#{ URI::escape(@canvas_page) }"
 
-    @signed_request = params[:signed_request] || "";
+    @signed_request = params[:signed_request] || ""
     
     logger.debug "Signed Request: #{ @signed_request }"
     
@@ -32,9 +32,9 @@ class CanvasController < ApplicationController
       "en"=> "en_US",
     }
     
-    default_locale = "en_US";
+    default_locale = "en_US"
     
-    fb_locale = @facebook_user_data['locale'];
+    fb_locale = @facebook_user_data['locale']
     
     logger.debug "fb_locale #{ fb_locale } lang_mapping #{ lang_mapping.inspect }."
     
@@ -55,54 +55,54 @@ class CanvasController < ApplicationController
   
   
   def create
-        @title = "Wack-A-Doo"
+    @title = "Wack-A-Doo"
 
-        @app_id = "127037377498922";
-        @canvas_page = "http://apps.facebook.com/wack-a-doo";
-        @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{ @app_id }&redirect_uri=#{ URI::escape(@canvas_page) }";
+    @app_id = "127037377498922"
+    @canvas_page = "http://apps.facebook.com/wack-a-doo"
+    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{ @app_id }&redirect_uri=#{ URI::escape(@canvas_page) }"
 
-        @signed_request = params[:signed_request] || "";
+    @signed_request = params[:signed_request] || ""
 
-        logger.debug "Signed Request: #{ @signed_request }"
+    logger.debug "Signed Request: #{ @signed_request }"
 
-        data = decode_facebook_hash(@signed_request)
+    data = decode_facebook_hash(@signed_request)
 
-    #    if !params[:code].blank?
-    #      data = ActiveSupport::JSON.decode(Base64.decode64(params[:code]))
-    #      logger.debug "Code: #{ code.inspect }"
-    #    end
+#    if !params[:code].blank?
+#      data = ActiveSupport::JSON.decode(Base64.decode64(params[:code]))
+#      logger.debug "Code: #{ code.inspect }"
+#    end
 
-        logger.debug "Facebook Data: #{data.inspect}."    
+    logger.debug "Facebook Data: #{data.inspect}."
 
-        @facebook_user = data.nil? || data["user_id"].blank? ? nil :  data["user_id"]    
-        @facebook_user_data = data.nil? ? nil : (data["user"] || {})
-        @referer = request.referer  || "facebook.canvas"
-        
-        lang_mapping = {
-          "de" => "de_DE",
-          "en" => "en_US",
-        }
+    @facebook_user = data.nil? || data["user_id"].blank? ? nil :  data["user_id"]
+    @facebook_user_data = data.nil? ? nil : (data["user"] || {})
+    @referer = request.referer  || "facebook.canvas"
 
-        default_locale = "en_US";
+    lang_mapping = {
+      "de" => "de_DE",
+      "en" => "en_US",
+    }
 
-        fb_locale = @facebook_user_data['locale'];
-        
-        logger.debug "fb_locale #{ fb_locale } lang_mapping #{ lang_mapping.inspect }."
+    default_locale = "en_US"
 
-        if !fb_locale.blank?
-          lang = fb_locale[0..1]
-          @locale = lang_mapping[lang]
-          logger.debug "locale #{ @locale } lang #{ lang }."
-        end
+    fb_locale = @facebook_user_data['locale']
 
-        @locale = default_locale    if @locale.nil?
-        logger.debug "after locale #{ @locale }."
-    
-        if !@facebook_user.nil?
-          access = IdentityProvider::Access.new(identity_provider_base_url: CLIENT_CONFIG['identity_provider_base_url'])
-          @access_data = access.obtain_access_token(@facebook_user)
-        end
-    
+    logger.debug "fb_locale #{ fb_locale } lang_mapping #{ lang_mapping.inspect }."
+
+    if !fb_locale.blank?
+      lang = fb_locale[0..1]
+      @locale = lang_mapping[lang]
+      logger.debug "locale #{ @locale } lang #{ lang }."
+    end
+
+    @locale = default_locale    if @locale.nil?
+    logger.debug "after locale #{ @locale }."
+
+    if !@facebook_user.nil?
+      access = IdentityProvider::Access.new(identity_provider_base_url: CLIENT_CONFIG['identity_provider_base_url'])
+      @access_data = access.obtain_access_token(@facebook_user)
+    end
+
     
     render action: "show"
   end
