c2894a3984a45ddee85eb1deeaaecfcfb439209d
Patrick <p@trick-fox.de>
2013-10-14 23:20:10 +0200

    Revert "debugging"
    This reverts commit a6b67cf49790835cfa455626f06422f40b2097ab.

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -5,16 +5,16 @@ Client::Application.routes.draw do
   scope "/client" do
     resource :canvas,  :only => [ :show, :create ]
     scope "(:locale)", :locale => /en|de/ do
-      #match :index, :to => 'client#show'
-      #root :to => 'client#show'
+      match :index, :to => 'client#show'
+      root :to => 'client#show'
     end
   end
 
   resource :client, :only => [ :create ]
 
-  #root :to => 'client#show'
-  #match :index, :to => 'client#show'
-  #match '/:locale' => 'client#show'
+  root :to => 'client#show'
+  match :index, :to => 'client#show'
+  match '/:locale' => 'client#show'
     
   
   # The priority is based upon order of creation:
