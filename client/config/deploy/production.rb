f54076e8665f379d2da531b730b735457cc8f1a7
Patrick Fox <patrick.fox@meinestadt.de>
2014-03-17 22:01:15 +0100

    config

--- client/config/deploy/production.rb
+++ client/config/deploy/production.rb
@@ -5,4 +5,4 @@ role :db,  "gs04.wack-a-doo.de", :primary => true        # This is where Rails m
 set :rails_env, 'production'
 set :port, 5775                                            # until 5775 is working!
 
-set :branch,    "master"
+set :branch,    "round4"
