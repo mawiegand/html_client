ddd09e0a33d677629bfecab847b4968e02f58a93
Marc Wißler <MarcWissler@minimac16.fritz.box>
2014-09-26 14:48:30 +0200

    Remove white background

--- client/app/assets/javascripts/awe/i18n/de_DE.js
+++ client/app/assets/javascripts/awe/i18n/de_DE.js
@@ -1460,7 +1460,12 @@ AWE.I18n.de_DE = function(module) {
           slogan: "Losung:",
           autoJoin: "Aufnahme:"
         }
-      }
+      },
+      move:{
+        heading: "Umziehen",
+        description: "Hier steht wie man umziehen kann.",
+        to: "nach"
+      },
     },
   }
   
