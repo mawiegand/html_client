6eeae2772cb074ed92e94c87dd785073f7e191a5
Sascha Lange <sascha@5dlab.com>
2014-11-18 11:00:06 +0100

    track platform facebook

--- client/app/assets/javascripts/wackadoo.js
+++ client/app/assets/javascripts/wackadoo.js
@@ -681,11 +681,6 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
       window.name = "empty";                                 // unset variables
       fbArgs = "empty";
 
-      Sample.setEndpoint("/psiori/event")
-      Sample.setAppToken("wad-rt82-fhjk-18");
-      Sample.sessionStart();
-      Sample.autoPing(30);
-
       var accessToken = null;
 
       if (!args || !args.accessToken) {
@@ -713,6 +708,18 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
 
       AWE.Facebook.isRunningInCanvas = AWE.Settings.fbRunInCanvas;
       AWE.Facebook.isFbPlayer = !!args.fbPlayerId;
+      
+      Sample.setEndpoint("/psiori/event")
+      Sample.setAppToken("wad-rt82-fhjk-18");
+      
+      if (AWE.Facebook.isRunningInCanvas)
+      {
+        Sample.setFacebookId(args.fbPlayerId || null);
+        Sample.setPlatform(Sample.PLATFORM_FACEBOOK);
+      }
+
+      Sample.sessionStart();
+      Sample.autoPing(30);
 
       AWE.Net.currentUserCredentials = AWE.Net.UserCredentials.create({
         access_token: accessToken,
