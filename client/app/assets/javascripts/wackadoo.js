203f426c252fdd33d4a39a9591475200254c4ad5
silas <silas@5dlab.com>
2014-11-28 13:02:49 +0100

    new fix for quest dialog resize and top bar resize

--- client/app/assets/javascripts/wackadoo.js
+++ client/app/assets/javascripts/wackadoo.js
@@ -845,8 +845,13 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
             || theRules[m].selectorText === ".modal-dialog-pane" 
             || theRules[m].selectorText === ".right-hud" 
             || theRules[m].selectorText === ".settlement-map-button" 
+            || theRules[m].selectorText === ".topbar-info-box.settlement" 
             || theRules[m].selectorText === ".shop-dialog-pane") {
             theRules[m].style.zoom = AWE.Settings.hudScale;
+          }
+          if (theRules[m].selectorText === '.topbar-info-box') {
+            theRules[m].style.top = 90*AWE.Settings.hudScale+'px';
+            theRules[m].style.marginLeft= -170*AWE.Settings.hudScale+'px';
           }
         }       
       }
