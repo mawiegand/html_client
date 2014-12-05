a3b61c37224d1d54c9f9621de5f3834027f567df
Radik Yusupov <radik@bam-local.aeriagames.com>
2014-12-05 16:09:07 +0100

    WAD-951

--- client/app/assets/javascripts/awe/UI/ember/armychangecreateviews.js
+++ client/app/assets/javascripts/awe/UI/ember/armychangecreateviews.js
@@ -124,7 +124,7 @@ AWE.UI.Ember = (function(module) {
       }
       // log("LIST", list, details);
       return list;
-    }.property('garrisonArmy.details.@each', 'otherArmy.details.@each', 'unitTypesChange').cacheable(),
+    }.property('garrisonArmy', 'otherArmy'/*'garrisonArmy.details.@each', 'otherArmy.details.@each', 'unitTypesChange'*/).cacheable(),
     
     unitDifferences: function() {
       var unitDifferences = {};
@@ -201,7 +201,7 @@ AWE.UI.Ember = (function(module) {
       }
       // log("LIST", list, details);
       return list;
-    }.property('garrisonArmy.details.@each', 'otherArmy.details.@each', 'unitTypesChange').cacheable(),
+    }.property('garrisonArmy'/*'garrisonArmy.details.@each', 'otherArmy.details.@each', 'unitTypesChange'*/).cacheable(),
 
     //other units for new army
     unitQuantities: function() {
