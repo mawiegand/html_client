6a641501f3fda6de64f22e10def0fa0226341aec
Daniel Band <dband@outlook.com>
2014-12-08 18:19:41 +0100

    PSIORI disable android-app tracking on html client

--- client/app/assets/javascripts/wackadoo.js
+++ client/app/assets/javascripts/wackadoo.js
@@ -362,7 +362,11 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
         AWE.Facebook.setDoneLoading();      // track loading time, iff running in canvas
 
         Sample.setUserId(identifier);
-        Sample.track('started', 'session');
+
+        if (Sample.getPlatform() != Sample.PLATFORM_ANDROID)
+        {
+            Sample.track('started', 'session');
+        }
 
         if (AWE.Config.CHAT_SHOW) {
           self.initChat();
@@ -810,8 +814,36 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
 
       var accessToken = null;
 
+      /** Set the pisori installtoken, sessiontoken and platform
+        * so that events don´t get tracked twice
+        */
+      var startupArgs = this.get('startupArguments');
+      var platform = startupArgs['platform'];
+
+      if (platform == Sample.PLATFORM_ANDROID)
+      {
+          var sessionToken = startupArgs['session_token'];
+          var installToken = startupArgs['install_token'];
+          if (sessionToken != "undefined" && sessionToken != null)
+          {
+              Sampl.setSessionToken(sessionToken);
+          }
+          if (installToken != "undefined" && installToken != null)
+          {
+              Sampl.setInstallToken(installToken);
+          }
+          if platform != "undefined" && platform != null)
+          {
+              Sampl.setPlatform(platform);
+          }
+      }
+
       if (!args || !args.accessToken) {
-        Sample.track('start_failed', { event_category: 'session'});
+        if (Sample.getPlatform() != Sample.PLATFORM_ANDROID)
+        {
+            Sample.track('start_failed', { event_category: 'session'});
+        }
+
 //      alert('FATAL ERROR: Invalid Credentials. Please contact the support staff.');
         document.location.href = AWE.Config.PORTAL_ROOT;
         return ;
@@ -888,8 +920,11 @@ window.WACKADOO = AWE.Application.MultiStageApplication.create(function() {
         styleSheet.insertRule(".shop-dialog-pane { height: 500px; margin-top: 100px; overflow: scroll; }", styleSheet.rules.length);
       }*/
 
-      Sample.sessionStart();
-      Sample.autoPing(30);
+      if (Sample.getPlatform() != Sample.PLATFORM_ANDROID)
+      {
+         Sample.sessionStart();
+         Sample.autoPing(30);
+      }
 
       AWE.Net.currentUserCredentials = AWE.Net.UserCredentials.create({
         access_token: accessToken,
