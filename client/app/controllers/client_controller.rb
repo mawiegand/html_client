69dca7c187524668c404af7aef88bc6da4104fc8
Jona Boeddinghaus <jona@5dlab.com>
2015-08-03 09:21:12 +0200

    implemented tracking pixel EGG

--- client/app/controllers/client_controller.rb
+++ client/app/controllers/client_controller.rb
@@ -4,12 +4,26 @@ class ClientController < ApplicationController
     @title = "Wack-A-Doo"
     
     @do_tracking  = params.has_key? "signup"
+    
+    @google_pixel = false
+    @crobo_pixel  = false
+    if params.has_key? "ref_id"
+      @google_pixel = params["ref_id"] == "0004-000" || params["ref_id"] == "0005-000"
+      @crobo_pixel  = params["ref_id"] == "0007-000"
+    end
   end
 
   def create
     @title = "Wack-A-Doo"
 
     @do_tracking  = params.has_key? "signup"
+    
+    @google_pixel = false
+    @crobo_pixel  = false
+    if params.has_key? "ref_id"
+      @google_pixel = params["ref_id"] == "0004-000" || params["ref_id"] == "0005-000"
+      @crobo_pixel  = params["ref_id"] == "0007-000"
+    end
 
     @fb_args = params['fb_args']
 
